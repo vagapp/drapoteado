29b7a6b389db10576f8c8a25937c303a8fc06bdf
Bardo-no <bardoafu@gmail.com>
2018-09-19 15:22:08 -0700

    update

--- src/pages/nuevacita-modal/nuevacita-modal.module.ts
+++ src/pages/nuevacita-modal/nuevacita-modal.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { NuevacitaModalPage } from './nuevacita-modal';
 import { CalendarModule } from "ion2-calendar";
+import { momentPicker } from "angular-moment-picker";
+import { DlDateTimePickerDateModule } from 'angular-bootstrap-datetimepicker';
 
 @NgModule({
   declarations: [
@@ -9,7 +11,8 @@ import { CalendarModule } from "ion2-calendar";
   ],
   imports: [
     IonicPageModule.forChild(NuevacitaModalPage),
-    CalendarModule
+    CalendarModule,
+    DlDateTimePickerDateModule
   ],
 })
 export class NuevacitaModalPageModule {}
