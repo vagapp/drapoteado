efe989c8c36df5bbb02a34f17f754e432a036c40
Bardo-no <bardoafu@gmail.com>
2020-03-18 17:47:20 -0700

    socket url

--- src/providers/base-url/base-url.ts
+++ src/providers/base-url/base-url.ts
@@ -13,10 +13,10 @@ export class BaseUrlProvider {
   //hostname:string = 'vmi118470.contaboserver.net/~drapp/';
   //hostname:string = '18.191.210.253/';
   //hostname:string = 'ec2-18-191-210-253.us-east-2.compute.amazonaws.com/';
-  hostname:string = 'www.drap.com.mx/';
+  hostname:string = 'www.tual.mx/';
   //websocketUrl:string = 'ws://vagapp.mx:8081/';
   //websocketUrl:string = 'ws://www.drap.com.mx:8081/';
-  websocketUrl:string = 'wss://www.drap.com.mx:8443/';
+  websocketUrl:string = 'wss://www.tual.mx:8443/';
   backend_tual_internas_full_name = 'https://bkint.tual.mx/';
   backend_tual_dev_full_name = 'https://bkdev.tual.mx/';
   backend_tual_pro_full_name = 'https://bkpro.tual.mx/';
